a8481e5f3c2683cd4293842101ed3a00f4f6afb3
seun <seunogunmolufirst1@gmail.com>
2022-01-01 14:51:49 +0100

    font change

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+  TextEditingController _controler = TextEditingController();
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,17 +49,45 @@ class Register extends StatelessWidget {
                           fontWeight: FontWeight.bold,
                           color: Color(0xFF044EBE))),
                 ),
-
               ),
               Padding(
                 padding: const EdgeInsets.only(left: 20.0),
-                child: Text("Create a free account and start a proper \nfinancial journey with PiggyVest", style: GoogleFonts.aclonica(textStyle: TextStyle(fontSize: 15,),),),
+                child: Text(
+                  "Create a free account and start a proper \nfinancial journey with PiggyVest",
+                  style: GoogleFonts.aclonica(
+                    textStyle: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
               ),
-               Padding(
-                 padding: const EdgeInsets.only(left: 20.0, top: 20.0),
-                 child: _buildText("Full Name"),
-               ),
-
+              SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 20.0),
+                child: _buildText("Full Name"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.2),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Full Name",
+                      hintStyle: TextStyle(
+                          color: Colors.grey.withOpacity(0.9),
+                          fontWeight: FontWeight.bold),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -60,9 +95,15 @@ class Register extends StatelessWidget {
     );
   }
 
-  Text _buildText(String text,) {
+  Text _buildText(
+    String text,
+  ) {
     return Text(
-      text, style: GoogleFonts.aclonica(textStyle: const TextStyle(fontSize: 15, color: Colors.black),),
+      text,
+      style: GoogleFonts.aclonica(
+        textStyle: const TextStyle(
+            fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
